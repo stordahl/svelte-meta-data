eacca4269d069c6965a6c3ae0f314fc22d4a6e7d
stordahl <jacob@blkcatstudio.com>
2021-01-07 21:34:27 -0600

    init

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,1 +1,52 @@
-<!-- your code here -->
+<script>
+  import { siteURL, siteAuthor, siteIndex } from './store'
+
+  export let meta_data = {
+    site_url: "https://fakeurl.com",
+    site_author: "Fake Author Name",
+    site_index: "/index",
+    title: undefined,
+    noindex: false,
+    nofollow: false,
+    description: undefined,
+    keywords: undefined,
+    canonical: undefined,
+    openGraph: undefined,
+    twitter: undefined,
+  };
+
+  siteURL.set(meta_data.site_url);
+  siteAuthor.set(meta_data.site_author);
+  siteIndex.set(meta_data.site_index);
+
+  const createArr = (obj) => {
+    let arr = Object.keys(obj)
+      .map(key => ({
+        key: key, 
+        data: obj[key]
+      }))
+    return arr
+  }
+
+  const arrayToUse = createArr(meta_data);
+</script>
+
+<svelte:head>
+  {#each arrayToUse as item}
+    {#if item.key == 'openGraph' || item.key == 'twitter'}
+      {#each createArr(item.data) as i}
+        <meta name={i.key} content={i.data}/>
+      {/each}
+    {:else if item.key == 'title'}
+      <title>{item.data}</title>
+		{:else if 
+			item.data !== undefined && 
+			item.data !== false && 
+			item.key !== "site_url" && 
+			item.key !== "site_author" && 
+			item.key !== "homepage"
+		}
+    	<meta name={item.key} content={item.data} />
+		{/if}
+  {/each}
+</svelte:head>
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store'
+
+export const siteURL = writable(null)
+export const siteAuthor = writable(null)
+export const siteIndex = writable(null)
